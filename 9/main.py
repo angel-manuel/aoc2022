3d50b11da0cffabd50e0e69ea94bd727e3cda5b2
angel-manuel <angel_manuel_martin@yahoo.es>
2022-12-09 12:49:50 +0100

    9 generic and part 2

--- 9/main.py
+++ 9/main.py
@@ -8,49 +8,63 @@ with open(sys.argv[1]) as f:
 
 tail_pos = set()
 
-head = (0, 0)
-tail = (0, 0)
-for line in lines:
-    dir, l = line.split()
+def move_tail(head, tail):
+    delta = (head[0] - tail[0], head[1] - tail[1])
 
-    print(line)
+    if abs(delta[0]) + abs(delta[1]) <= 2 and abs(delta[0]) <= 1 and abs(delta[1]) <= 1:
+        return tail
+    
+    if abs(delta[0]) == 2 and delta[1] == 0:
+        tail = (tail[0] + delta[0] // 2, tail[1])
 
-    for i in range(int(l)):
-        if dir == 'R':
-            head = (head[0], head[1] + 1)
-        elif dir == 'U':
-            head = (head[0] - 1, head[1])
-        elif dir == 'L':
-            head = (head[0], head[1] - 1)
-        elif dir == 'D':
-            head = (head[0] + 1, head[1])
-        
-        delta = (head[0] - tail[0], head[1] - tail[1])
+    if abs(delta[1]) == 2 and delta[0] == 0:
+        tail = (tail[0], tail[1] + delta[1] // 2)
+    
+    if abs(delta[0]) == 2 and abs(delta[1]) == 1:
+        tail = (tail[0] + delta[0] // 2, tail[1] + delta[1])
 
-        if abs(delta[0]) + abs(delta[1]) <= 2 and abs(delta[0]) <= 1 and abs(delta[1]) <= 1:
-            pprint({
-                'head': head,
-                'tail': tail
-            })
+    if abs(delta[1]) == 2 and abs(delta[0]) == 1:
+        tail = (tail[0] + delta[0], tail[1] + delta[1] // 2)
+
+    if abs(delta[0]) == 2 and abs(delta[1]) == 2:
+        tail = (tail[0] + delta[0] // 2, tail[1] + delta[1] // 2)
+    
+    return tail
+
+def move_string(slist, commands):
+    tail_pos = set()
+
+    tail_pos.add(slist[-1])
+
+    for command in commands:
+        dir, l = command.split()
+
+        for t in range(int(l)):
+            head = slist[0]
+
+            if dir == 'R':
+                head = (head[0], head[1] + 1)
+            elif dir == 'U':
+                head = (head[0] - 1, head[1])
+            elif dir == 'L':
+                head = (head[0], head[1] - 1)
+            elif dir == 'D':
+                head = (head[0] + 1, head[1])
+            
+            slist[0] = head
+
+            for i in range(1, len(slist)):
+                slist[i] = move_tail(slist[i-1], slist[i])
+            
+            tail = slist[-1]
             tail_pos.add(tail)
-            continue
+    
+    return tail_pos
 
-        if abs(delta[0]) == 2 and delta[1] == 0:
-            tail = (tail[0] + delta[0] // 2, tail[1])
-
-        if abs(delta[1]) == 2 and delta[0] == 0:
-            tail = (tail[0], tail[1] + delta[1] // 2)
-        
-        if abs(delta[0]) == 2 and abs(delta[1]) == 1:
-            tail = (tail[0] + delta[0] // 2, tail[1] + delta[1])
-
-        if abs(delta[1]) == 2 and abs(delta[0]) == 1:
-            tail = (tail[0] + delta[0], tail[1] + delta[1] // 2)
-        
-        pprint({
-            'head': head,
-            'tail': tail
-        })
-        tail_pos.add(tail)
+tail_pos = move_string([(0, 0)] * 2, lines)
 
 print(len(tail_pos))
+
+tail_pos_10 = move_string([(0, 0)] * 10, lines)
+
+print(len(tail_pos_10))
