6799656a1a3cc0bfada4b78438dad45c35d911ec
angel-manuel <angel_manuel_martin@yahoo.es>
2022-12-15 23:00:07 +0100

    15

--- 15/main.py
+++ 15/main.py
@@ -109,3 +109,63 @@ def count_from_imp_seg(y, segments):
 
 print(count_from_imp_seg(10, imp_seg_at_y(10)))
 print(count_from_imp_seg(2000000, imp_seg_at_y(2000000)))
+
+def subtract_segments(s1, s2):
+    s1a, s1b = s1
+    s2a, s2b = s2
+
+    ret = []
+
+    if s1a < s2a:
+        ret.append((s1a, min(s2a - 1, s1b)))
+    
+    if s2b < s1b:
+        ret.append((max(s1a, s2b + 1), s1b))
+    
+    return ret
+
+
+def possible_in_area(mins, maxs):
+    min_x, min_y = mins
+    max_x, max_y = maxs
+
+    ret = []
+
+    for y in range(min_y, max_y + 1):
+        possible = [(min_x, max_x)]
+
+        for imp_seg in imp_seg_at_y(y):
+            if not possible:
+                break
+
+            new_possible = []
+
+            for possible_seg in possible:
+                new_possible.extend(subtract_segments(possible_seg, imp_seg))
+
+            possible = new_possible
+        
+        if possible:
+            ret.append((y, possible))
+    
+    return ret
+
+area_pos = possible_in_area((0, 0), (20, 20))
+
+if DEBUG:
+    pprint(area_pos)
+
+if len(lines) == 32:
+    area_pos = possible_in_area((0, 0), (4000000 , 4000000))
+
+    if DEBUG:
+        pprint(area_pos)
+
+pos = (area_pos[0][1][0][0], area_pos[0][0])
+
+if DEBUG:
+    print(pos)
+
+tuning_freq = pos[0]*4000000 + pos[1]
+
+print(tuning_freq)
